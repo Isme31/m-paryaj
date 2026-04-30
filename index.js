3084684aa023f4e169ef15c6cf6c8562e76b3f5e
Isme31 <huguesbelardisme47@gmail.com>
2026-04-30 09:09:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
@@ -8,25 +7,29 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Sèvi fichye HTML yo ki nan folder "public" la (si w genyen l)
-app.use(express.static(path.join(__dirname, 'public')));
+// Liy sa a ap sèvi tout fichye ki nan rasin pwojè a (HTML, CSS, JS)
+app.use(express.static(__dirname));
 
-// Si w pa gen folder public, li pral chèche index.html nan rasin lan
+// Liy sa a ap voye index.html bay moun k ap vizite sit la
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
-// Jesyon koneksyon jwè yo ak Socket.io
+// Jesyon Socket.io pou jwèt la
 io.on('connection', (socket) => {
   console.log('Yon jwè konekte: ' + socket.id);
+
+  socket.on('mouvman', (data) => {
+    // Voye mouvman an bay tout lòt jwè yo
+    socket.broadcast.emit('mouvman', data);
+  });
 
   socket.on('disconnect', () => {
     console.log('Yon jwè dekonekte');
   });
-
-  // Ou ka ajoute lòt fonksyon pou jwèt la isit la (mouvman, genyen, elatriye)
 });
 
+// Render ap toujou bay yon pò nan "process.env.PORT"
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Sèvè a ap kouri sou pò ${PORT}`);
